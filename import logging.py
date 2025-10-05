2207e8161e3c63c8ea766335590c5da5ee62fd28
Tamikxss <zamiatintimofey@gmail.com>
2025-10-05 15:50:31 +0300

    homework8

--- import logging.py
+++ import logging.py
@@ -1,5 +1,6 @@
 import logging
-from datetime import datetime
+import io
+import unittest
 
 logging.basicConfig(
     format='%(asctime)s - %(message)s',
@@ -8,3 +9,25 @@ logging.basicConfig(
 )
 
 logging.info('Это информационное сообщение')
+
+
+# --- тесты ---
+class TestLogging(unittest.TestCase):
+    def test_logging_message(self):
+        stream = io.StringIO()
+        handler = logging.StreamHandler(stream)
+        formatter = logging.Formatter('%(asctime)s - %(message)s', datefmt='%Y-%m-%d')
+        handler.setFormatter(formatter)
+
+        logger = logging.getLogger("test_logger")
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
+
+        logger.info("Тестовое сообщение")
+        output = stream.getvalue()
+
+        self.assertIn("Тестовое сообщение", output)
+
+
+if __name__ == "__main__":
+    unittest.main()
